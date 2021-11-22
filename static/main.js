77b0c768de50829793213ee338108343433cadff
Utkarsha Nehe <utkarshanehe@gmail.com>
2021-11-22 23:56:36 +0530

    Add post detail page

---
+++ static/main.js
@@ -0,0 +1,9 @@
+function showCommentArea() {
+    var commentArea = document.getElementById("comment-area");
+    commentArea.classList.remove("hide");
+}
+
+function showCommentReplyArea() {
+    var replyArea = document.getElementById("reply-area");
+    replyArea.classList.remove("hide");
+}
