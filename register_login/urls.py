bb60f4b4e321a1dad97ad98231a3b7783911d843
Utkarsha Nehe <utkarshanehe@gmail.com>
2021-11-26 00:31:56 +0530

    Add login functionality to the app

--- register_login/urls.py
+++ register_login/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import signup
+from .views import signup, signin
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
+    path('signin/', signin, name='signin'),
 ]
--- register_login/views.py
+++ register_login/views.py
@@ -1,6 +1,6 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def signup(request):
@@ -12,9 +12,30 @@ def signup(request):
             new_user = form.save()
             login(request, new_user)
             return redirect("index")
+
     context.update({
         "form": form,
         "title": "Signup",
     })
     return render(request, 'register_login/signup.html', context)
+
+
+def signin(request):
+    context = {}
+    form = AuthenticationForm(request, data=request.POST)
+
+    if request.method == "POST":
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("index")
+
+    context.update({
+        "form": form,
+        "title": "Signin",
+    })
+    return render(request, "register_login/signin.html", context)
 
