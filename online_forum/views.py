29e4546241a5c8a5c0014b6543d53a772bf1fea4
Utkarsha Nehe <utkarshanehe@gmail.com>
2021-11-26 19:13:22 +0530

    Add pagination to the posts page

--- online_forum/views.py
+++ online_forum/views.py
@@ -3,20 +3,51 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Author, Category, Post, Comment, Reply
 from .utils import update_views
 from .forms import CreatePostForm
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 def index(request):
     forums = Category.objects.all()
-    context = {'forums': forums}
+    number_of_posts = Post.objects.all().count()
+    number_of_users = User.objects.all().count()
+    number_of_forums = forums.count()
+
+    try:
+        last_post = Post.objects.latest("timestamp")
+    except:
+        last_post = []
+
+    context = {
+        'forums': forums,
+        'number_of_posts': number_of_posts,
+        'number_of_users': number_of_users,
+        'number_of_forums': number_of_forums,
+        'last_post': last_post,
+        'title': 'Forum App'
+    }
     return render(request, 'index.html', context)
 
 
 def show_posts(request, slug):
     category = get_object_or_404(Category, slug=slug)
     posts = Post.objects.filter(approved=True, categories=category)
+
+    # Pagination for the posts
+    paginator = Paginator(posts, 10)
+    page = request.GET.get("page")
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     context = {
         'posts': posts,
         'forum': category,
+        'title': category.title + ' Posts',
     }
     return render(request, 'posts.html', context)
 
@@ -42,11 +73,10 @@ def show_post_detail(request, slug):
         comment_object.replies.add(new_reply.id)
 
     context = {
-        'post': post
+        'post': post,
+        'title': post.title,
     }
     update_views(request, post)
-    print(post.title)
-    print(post.comments)
     return render(request, 'post_detail.html', context)
 
 
