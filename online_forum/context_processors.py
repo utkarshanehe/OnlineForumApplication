a6a994aa184b5a4a9381bd835a8109455d24c10a
Utkarsha Nehe <utkarshanehe@gmail.com>
2021-11-27 19:13:26 +0530

    Add styling to navbar of the app

--- online_forum/context_processors.py
+++ online_forum/context_processors.py
@@ -7,15 +7,7 @@ def search(request):
 
     if "search-form" in request.GET:
         search_query = request.GET.get("search-query")
-
-        # Filter starts here
-        search_box = request.GET.get("search-box")
-        if search_box == "Descriptions":
-            search_result = posts.filter(content__icontains=search_query)
-        else:
-            search_result = posts.filter(title__icontains=search_query)
-        # Ends here
-
+        search_result = posts.filter(title__icontains=search_query)
         search_context = {
             "search_result": search_result,
             "search_query": search_query,
